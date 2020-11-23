885f68f3844bc83eb7a1fb99574ef37a24892e32
damshaw <damshaw@gmail.com>
2020-11-23 17:57:01 +1100

    working on the filtering"

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import "./App.scss";
 import Filter from "./Filter";
 import useFetch from "./hooks/useFetch";
 
-function App() {
-  // const ROKEBY_CAT_ID = "107,176,108";
+function App({ faqData }) {
+  const ROKEBY_CAT_ID = "107,176,108";
   // const COCOBELLA_CAT_ID = "107,176,108";
   // const IMPRESSED_CAT_ID = "107,176,108";
   // const APP_CAT_ID = "107,176,108";
-  const API_URL = "https://faqs.madegroup.com/wp-json/wp/v2/posts/";
-  // const PER_PAGE = "per_page=100";
-  // const CAT = "categories";
+  const PER_PAGE = "per_page=100";
+  const CAT = "categories";
   // const TAG = "tags";
   // const POST = "posts";
-
-  const [menuOpen, setMenuOpen] = useState(false);
   // const [postsData, setPostsData] = useState("");
   // const [filter, setFilter] = useState("");
 
@@ -22,18 +19,31 @@ function App() {
   //    setPostsData(`${CAT}=${APP_CAT_ID}&${PER_PAGE}`)
   //   //&tags=169
   //   : setPostsData(`${CAT}=${ROKEBY_CAT_ID}=${TAG}=${APP_CAT_ID}&${PER_PAGE}`);
+
+  const API_URL = "https://faqs.madegroup.com/wp-json/wp/v2/posts/?" + PER_PAGE + "&" + CAT + "=" + ROKEBY_CAT_ID;
+  const faqIds = faqData;
+  const faqIdsArray = faqIds.split(",").map(function (item) {
+    return parseInt(item, 10);
+  });
+  const [menuOpen, setMenuOpen] = useState(false);
   const posts = useFetch(API_URL);
+
+  console.log("faqData", faqData);
+  console.log("faqIdsArray", faqIdsArray);
   console.log("posts", posts);
+
   return (
     <div className="App">
-      <Filter />
       <button onClick={() => setMenuOpen(!menuOpen)}>Click me</button>
       <div className="faq-wrapper flex-grid" id="faq-list">
         {posts &&
           posts
-            .filter((post) => {
-              return post.categories.includes(221);
-            })
+
+            // .filter((post) => {
+            //   // return post.categories.includes(faqIdsArray);
+            //   const res = post.categories.filter((f) => faqData.includes(f));
+            //   console.log(res);
+            // })
             .map((post) => (
               <div className="card">
                 <h4>{post.title.rendered}</h4>
